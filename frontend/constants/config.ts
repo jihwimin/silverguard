d81159056e3c495a9100d46e1e8f38a1eb7590a6
jihwimin <jihwimin@gmail.com>
2026-02-28 17:21:11 -0600

    diagnosis page done

--- frontend/constants/config.ts
+++ frontend/constants/config.ts
@@ -1,2 +1,4 @@
 // constants/Config.ts
 export const BASE_URL = "https://knvpzmfq-8000.usw3.devtunnels.ms";
+// frontend/constants/config.ts
+export const BASE_DETECTOR_URL = "https://q4jtmh26-8000.usw3.devtunnels.ms";
